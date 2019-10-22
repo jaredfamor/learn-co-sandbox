170cd273c89b1f17a0d2208497b4df8ae765cf34
William Famor <jaredfamor@gmail.com>
2019-10-22 01:57:11 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,12 @@
+Code before if...end
+code inside
+Code after if...end
+Let's go outside!
+Enjoy the fine day!
+Oh, and always wear sunscreen!
+If chance of rain is .0000001 pack a sun shelter
+If chance of rain is 0.2 Pack a sun shelter
+If chance of rain is 0.2000001 Pack a sun shelter
+If chance of rain is 0.3 Pack an umbrella
+If chance of rain is 0.9 Stay home and read Hegel
+If chance of rain is -23 Pack a sun shelter!
